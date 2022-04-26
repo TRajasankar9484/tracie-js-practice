c7a63e2154d6925fd84ed266c0ec76aace602dce
Tracie <tracie@ip-192-168-1-64.us-west-2.compute.internal>
2022-04-26 12:58:41 -0700

    Code for second challenge

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 alert("hey!");
 
-// creates new div element 
-let div = document.createElement('div');
-div.style.width = '50px';
-div.style.height = '50px';
-document.body.appendChild(div);
+// creates new div element and names it 'Board'
+let Board = document.createElement('div');
+Board.setAttribute('id', 'Board');
+document.body.appendChild(Board);
+
+for (let i = 0; i < 10; i++) {
+    let innerDiv = document.createElement('div');
+    innerDiv.style.width = '50px';
+    innerDiv.style.height = '50px';
+    Board.appendChild(innerDiv);
+}
