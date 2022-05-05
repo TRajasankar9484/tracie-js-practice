0e6ee029cdf982b358bde27f5ab7661e23d8d2ce
Tracie <tracie@ip-192-168-1-200.us-west-2.compute.internal>
2022-05-05 15:36:50 -0700

    Code for fourth challenge

--- javascript.js
+++ javascript.js
@@ -6,12 +6,17 @@ document.body.appendChild(Board);
 // Inserts 8 x 8 rows of divs inside of 'Board'
 for (let i = 0; i < 8; i++) {
     let rowDiv = document.createElement('div');
+    rowDiv.classList.add('row')
     document.body.appendChild(rowDiv);
     for (let j = 0; j < 8; j++) {
         let boxDiv = document.createElement('div');
         boxDiv.style.width = '50px';
         boxDiv.style.height = '50px';
         boxDiv.classList.add('box');
+        boxDiv.addEventListener("mouseover", function () {
+            boxDiv.style.backgroundColor = 'black';
+        })
         rowDiv.appendChild(boxDiv);
+
     }
 }
