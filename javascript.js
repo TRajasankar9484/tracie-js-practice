f29e45055416232370e46fdc549a7700cad8efb5
Tracie <tracie@ip-192-168-1-200.us-west-2.compute.internal>
2022-05-25 12:08:41 -0700

    Code Refactor

--- javascript.js
+++ javascript.js
@@ -1,22 +1,33 @@
-// creates new div element and names it 'Board'
-let Board = document.createElement('div');
-Board.setAttribute('id', 'Board');
-document.body.appendChild(Board);
+const createBoard = function () {
+    let Board = document.createElement('div');
+    Board.setAttribute('id', 'Board');
+    document.body.appendChild(Board);
+};
 
-// Inserts 8 x 8 rows of divs inside of 'Board'
-for (let i = 0; i < 8; i++) {
+const createRow = function () {
     let rowDiv = document.createElement('div');
     rowDiv.classList.add('row')
     document.body.appendChild(rowDiv);
-    for (let j = 0; j < 8; j++) {
-        let boxDiv = document.createElement('div');
-        boxDiv.style.width = '50px';
-        boxDiv.style.height = '50px';
-        boxDiv.classList.add('box');
-        boxDiv.addEventListener("mouseover", function () {
-            boxDiv.style.backgroundColor = 'black';
-        })
-        rowDiv.appendChild(boxDiv);
+    return rowDiv;
+};
 
-    }
-}
+const createBox = function (row) {
+    let boxDiv = document.createElement('div');
+    boxDiv.style.width = '50px';
+    boxDiv.style.height = '50px';
+    boxDiv.classList.add('box');
+    boxDiv.addEventListener("mouseover", function () {
+        boxDiv.style.backgroundColor = 'black';
+    })
+    row.appendChild(boxDiv);
+};
+
+
+createBoard();
+
+for (let i = 0; i < 8; i++) {
+    let rowDiv = createRow();
+    for (let j = 0; j < 8; j++) {
+        createBox(rowDiv);
+    };
+};
